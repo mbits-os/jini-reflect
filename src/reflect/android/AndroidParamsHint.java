4af0378e27768b8169ad8db489d87ebfb8cf8b15
Marcin Zdun <mzdun@midnightbits.com>
2013-02-14 21:25:01 +0100

    Moving API to a place called only once

--- src/reflect/android/AndroidParamsHint.java
+++ src/reflect/android/AndroidParamsHint.java
@@ -9,10 +9,17 @@ public class AndroidParamsHint extends SourceCodeParamsHint {
 
 	public static final class HintCreator implements ParamsHint.HintCreator {
 
-		private String m_sdk;
+		private File m_sdk;
 
-		HintCreator(String sdk) {
-			m_sdk = sdk;
+		HintCreator(String api) {
+			final String androidSDK = System.getenv("ANDROID_SDK");
+			if (androidSDK == null)
+			{
+				System.out.println("Environment variable ANDROID_SDK is missing.");
+				return;
+			}
+			final File SDK = new File(androidSDK);
+			m_sdk = new File(SDK, "sources" + File.separator + "android_" + api);
 		}
 		@Override public ParamsHint createHint() {
 			return new AndroidParamsHint(m_sdk);
@@ -22,17 +29,7 @@ public class AndroidParamsHint extends SourceCodeParamsHint {
 
 	private File m_api;
 
-	public AndroidParamsHint(String api) {
-		final String androidSDK = System.getenv("ANDROID_SDK");
-		if (androidSDK == null)
-		{
-			System.out.println("Environment variable ANDROID_SDK is missing.");
-			return;
-		}
-		File SDK = new File(androidSDK);
-		m_api = new File(SDK, "sources" + File.separator + "android_" + api);
-	}
-
+	public AndroidParamsHint(File api) { m_api = api; }
 	@Override protected File getSourceRoot(String className) { return m_api; }
 
 }
