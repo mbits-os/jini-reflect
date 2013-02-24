c84781ed763b0028ad2264fde0889f999d0cc757
Marcin Zdun <mzdun@midnightbits.com>
2013-02-24 08:39:36 +0100

    Bitmap locking mechanism

--- plugin-android-reflect/src/main/java/reflect/android/patches/BitmapPatch.java
+++ plugin-android-reflect/src/main/java/reflect/android/patches/BitmapPatch.java
@@ -13,7 +13,13 @@ public class BitmapPatch extends Patch {
 
 	@Override
 	public void onNamespaceStart(PrintStream out) {
-		out.println("\t//locking class goes here");
+		out.println("\tstruct BitmapLockInfo");
+		out.println("\t{");
+		out.println("\t\tAndroidBitmapInfo m_info;");
+		out.println("\t\tuint8_t*          m_pixels;");
+		out.println("\t\tBitmapLockInfo(): m_pixels(NULL) {}");
+		out.println("\t};");
+		out.println();
 	}
 
 	@Override
@@ -22,5 +28,22 @@ public class BitmapPatch extends Patch {
 			return;
 		out.println();
 		out.print(indent); out.println("//locking goes here");
+		out.print(indent); out.println("BitmapLockInfo lock()");
+		out.print(indent); out.println("{");
+		out.print(indent); out.println("\tJNIEnv* env = jni::Env();");
+		out.print(indent); out.println("\tBitmapLockInfo bli;");
+		out.println();
+		out.print(indent); out.println("\tint ret = AndroidBitmap_getInfo(env, m_this, &bli.m_info);");
+		out.print(indent); out.println("\tif (ret != ANDROID_BITMAP_RESULT_SUCCESS) return bli;");
+		out.print(indent); out.println("\tif (bli.m_info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) return bli;");
+		out.print(indent); out.println("\tret = AndroidBitmap_lockPixels(env, m_this, (void**)&bli.m_pixels);");
+		out.print(indent); out.println("\tif (ret != ANDROID_BITMAP_RESULT_SUCCESS) bli.m_pixels = NULL;");
+		out.print(indent); out.println("\treturn bli;");
+		out.print(indent); out.println("}");
+		out.println();
+		out.print(indent); out.println("void unlock()");
+		out.print(indent); out.println("{");
+		out.print(indent); out.println("\tAndroidBitmap_unlockPixels(jni::Env(), m_this);");
+		out.print(indent); out.println("}");
 	}
 }
