5980724974a079de669457fd3ff55a8c76c2dd19
Marcin Zdun <mzdun@midnightbits.com>
2013-02-27 06:46:19 +0100

    Searchbox colors and home link

--- reflect-base/template/components/masthead.cs
+++ reflect-base/template/components/masthead.cs
@@ -2,8 +2,15 @@
 <div id="header">
     <nav>
         <?cs if:project.name ?>
-        <h1><?cs var:project.name ?></h1>
+        <h1><a id="linkHome" href="<?cs var:toroot ?>jini-reflect/"><span>Home</span></a>&nbsp;&raquo; <?cs var:project.name ?></h1>
         <?cs /if ?>
+	<div id="headerTools">
+            <?cs call:default_search_box() ?>
+            <?cs if:reference && reference.apilevels ?>
+                <?cs call:default_api_filter() ?>
+
+            <?cs /if ?>
+	</div>
     </nav>
 </div><!-- header -->
 
